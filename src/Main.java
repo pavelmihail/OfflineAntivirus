31d9e53edc21ad7da01cae398d6904c8f28f5119
Mihai <valmi2009@gmail.com>
2022-11-13 10:21:51 +0200

    Manage input

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //define variables
+        Scanner sc = new Scanner(System.in);
+        //scan/check
+        String action = "";
+        String key;
+        //path we apply action to
+        String path;
+
+        //choose between scan an check
+        System.out.print("Choose action (scan/check): ");
+        while (!(Objects.equals(action, "scan") || Objects.equals(action, "check"))){
+            action = sc.nextLine();
+            if(!(Objects.equals(action, "scan") || Objects.equals(action, "check"))) {
+                System.out.println("The option you chose is not correct");
+                System.out.print("Choose action (scan/check): ");
+            }
+        }
+        System.out.println(action + " was selected");
+
+        System.out.print("Choose key: ");
+        key = sc.nextLine();
+        System.out.println("Chosen key :" + key);
+
+        System.out.print("Scan path: ");
+        path = sc.nextLine();
+        System.out.println("Path to be scanned :" + path);
+
     }
 }
